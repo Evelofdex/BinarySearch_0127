2740bc9a984b9e7f6978eebcdd1ae4d2b8fd8c00
Evelofdex <evelofdex@gmail.com>
2026-04-16 11:22:26 +0700

    benerin error

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -45,13 +45,13 @@ void bubbleSortArray(){
 
 void display(){
     cout << "\n=======================================\n";
-    cout << " Elemen Array Setelah Diurutkan (Asc   \n)";
+    cout << " Elemen Array Setelah Diurutkan (Asc) \n";
     cout  << "========================================\n";
 
     for (int j = 0; j < nPanjang; j++){
         cout << element[j];
         if (j < nPanjang - 1){
-            cout >> " -> ;";
+            cout << " -> ;";
         }
     }
     cout << endl;
