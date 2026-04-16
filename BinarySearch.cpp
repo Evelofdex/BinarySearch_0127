466b78ee0a2d60a3f8f5a6712936f34b33abb0f6
Evelofdex <evelofdex@gmail.com>
2026-04-16 11:01:31 +0700

    menambah didalm binary search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -74,7 +74,14 @@ void binarySearch(){
             int mid = (low + high) / 2;
 
             if (element[mid] == x){
-
+                cout << "\n[oke] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x < element[mid]){
+                high = mid - 1;
+            }
+            if (x > element[mid]){
+                low = mid + 1;
             }
         } while()
 
