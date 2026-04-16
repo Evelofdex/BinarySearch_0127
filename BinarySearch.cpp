ffd3df86f2d6c78d2f39631a6482b8a693c0f551
Evelofdex <evelofdex@gmail.com>
2026-04-16 10:27:15 +0700

    Lanjutan isi dari prosedur input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -15,8 +15,17 @@ void input(){
             break;
         }
         else{
-            
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi. \n";
         }
+    }
+
+    cout << "\n=================================\n";
+    cout << "       Masukan Elemen Array        \n";
+    cout << "===================================\n";
+
+    for (int i = 0; i < nPanjang; i++){
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
     }
 }
 
